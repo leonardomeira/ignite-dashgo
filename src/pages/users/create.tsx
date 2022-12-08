312145e3b98e89385156bb50325c1cbaea229bbb
Leonardo Meira <meiraleoabr@gmail.com>
2022-12-08 14:12:44 -0300

    improve Create Users page responsiveness

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,16 +10,8 @@ export default function CreateUser() {
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
-                <Box
-                    flex="1"
-                    borderRadius={9}
-                    bg="gray.800"
-                    p="8"
-                >
-                    <Heading
-                        size="lg"
-                        fontWeight="normal"
-                    >
+                <Box flex="1" borderRadius={9} bg="gray.800" p={["6", "8"]}>
+                    <Heading size="lg" fontWeight="normal">
                         Criar usuário
                     </Heading>
 
@@ -27,12 +19,12 @@ export default function CreateUser() {
 
                     <VStack spacing="8">
 
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="name" label="Nome completo" />
                             <Input name="email" type="email" label="E-mail" />
                         </SimpleGrid>
 
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="password" type="password" label="Senha" />
                             <Input name="passwordConfirmation" type="password" label="Confirmação da senha" />
                         </SimpleGrid>
