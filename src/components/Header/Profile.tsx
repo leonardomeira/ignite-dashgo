f7bd933f71d4703ac43ef5bd046295d93eef07d2
Leonardo Meira <meiraleoabr@gmail.com>
2022-12-08 13:25:06 -0300

    make Header component responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,32 +1,26 @@
-import { Avatar, Box, Flex, HStack, Icon, Text } from "@chakra-ui/react";
-import { RiNotificationLine, RiUserAddLine } from "react-icons/ri";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+};
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
-        <Flex
-            align="center"
-            ml="auto"
-        >
+        <Flex align="center" ml="auto">
 
             <Flex align="center">
-                <Box
-                    mr="4"
-                    textAlign="right"
-                >
-                    <Text>Leonardo Meira</Text>
-                    <Text
-                        color="gray.300"
-                        fontSize="small"
-                    >
-                        meiraleoabr@gmail.com
-                    </Text>
-                </Box>
 
-                <Avatar
-                    size="md"
-                    name="Leonardo Meira"
-                    src="https://github.com/leonardomeira.png"
-                />
+                {showProfileData && (
+                    <Box mr="4" textAlign="right">
+                        <Text>Leonardo Meira</Text>
+                        <Text color="gray.300" fontSize="small">
+                            meiraleoabr@gmail.com
+                        </Text>
+                    </Box>
+                )}
+                
+
+                <Avatar size="md" name="Leonardo Meira" src="https://github.com/leonardomeira.png" />
             </Flex>
         </Flex>
     );
